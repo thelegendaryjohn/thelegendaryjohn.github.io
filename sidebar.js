5f2cd0dd7f5ec0db368d30f7de4b78a497e0a9ee
The Legendary John <legendaryjohn2005@gmail.com>
2024-03-22 15:41:19 +0700

    Add more logics to sidebar

--- sidebar.js
+++ sidebar.js
@@ -1,20 +1,19 @@
-function openNav() {
-    document.getElementById("sidebar").style.width = "190px";
-    document.getElementById("main").style.marginLeft = "190px";
-}
- 
-function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+const toggleNav = () => {
+    console.log("Toggle navbar");
+    document.querySelector("#sidebar").classList.toggle("toggleSidebar");
 }
 
-// Event listener to close the sidebar when clicking outside
-document.addEventListener('click', function(event) {
-    const sidebar = document.getElementById("sidebar");
-    const main = document.getElementById("main");
-    
-    // Check if the click target is not within the sidebar or main content
-    if (!sidebar.contains(event.target) && main.contains(event.target)) {
-        closeNav();
-    }
-});
+const closeNav = () => {
+    console.log("Close navbar");
+    document.querySelector("#sidebar").classList.remove("toggleSidebar");
+}
+
+const detectIgnoredClass = (event) => {
+    event.stopPropagation();
+}
+
+document.querySelector("#menu-btn").addEventListener("click", toggleNav);
+document.querySelector("body").addEventListener("click", closeNav);
+document.querySelectorAll(".ignoreSidebarToggle").forEach((e) => {
+    e.addEventListener("click", detectIgnoredClass);
+})
