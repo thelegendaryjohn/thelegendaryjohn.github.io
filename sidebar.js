d50268ea74ff1aad6088bf7a0db13397a9853fc6
The Legendary John <legendaryjohn2005@gmail.com>
2024-03-22 23:06:01 +0700

    Add menu button animation

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,39 @@
+let navbarClosed = true;
+
+// const menuBtn = document.getElementById('menu-btn');
+// const animateElements = menuBtn.getElementsByTagName("animate");
+
+// // Trigger the animations programmatically
+// menuBtn.addEventListener('click', function() {
+//   for (let i = 0; i < animateElements.length; i++) {
+//     animateElements[i].beginElement();
+//   }
+// });
+
+const openAnim = () => {
+    document.querySelector("#start").beginElement();
+    navbarClosed = false;
+    console.log("Open animation");
+}
+
+const closeAnim = () => {
+    document.querySelector("#reverse").beginElement();
+    navbarClosed = true;
+    console.log("Close animation");
+}
+
 const toggleNav = () => {
     console.log("Toggle navbar");
     document.querySelector("#sidebar").classList.toggle("toggleSidebar");
+
+    (navbarClosed) ? openAnim() : closeAnim();
 }
 
 const closeNav = () => {
     console.log("Close navbar");
     document.querySelector("#sidebar").classList.remove("toggleSidebar");
+
+    if (!navbarClosed) { closeAnim() }
 }
 
 const detectIgnoredClass = (event) => {
