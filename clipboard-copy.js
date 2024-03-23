304043bdee0fdbc8641cf3887035810b203d3c83
The Legendary John <legendaryjohn2005@gmail.com>
2024-03-24 03:12:45 +0700

    Adding to  section. Implement copying to clipboard feature

---
+++ clipboard-copy.js
@@ -0,0 +1,7 @@
+const copyDiscordHandle = () => {
+     // Copy the text
+    navigator.clipboard.writeText("voca_johnni");
+  
+    // Alert the copied text
+    alert("Copied to clipboard");
+}
--- dev-mode-toggle.js
+++ dev-mode-toggle.js
@@ -5,9 +5,14 @@ const handleClick = () => {
   console.log(clickCount);
   
   if (clickCount == 3) {
-    document.querySelector(".development-feature").classList.toggle("hidden"); 
+    document.querySelectorAll(".development-feature").forEach(e => {
+      e.classList.toggle("hidden");
+    });
     clickCount = 0;
   }
 };
 
 document.querySelector("#header-logo").addEventListener("click", handleClick)
+
+// let agent = navigator.userAgent;
+// document.querySelector("#user-agent").innerHTML = agent;
